760b74b5c0644e60c7a1695fd080af0d9d444f4e
Sergey <okulov.sergej2016@yandex.ru>
2024-05-18 16:36:07 +0500

    Создали метод, который заполняет данные с клавиатуры о пользователе

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿static (string name, string lastName, int age, bool haveAPet, int petNumber, string[] pets, int favColorNumber, string[] favColors) EnterUser()
+{
+    (string name, string lastName, int age, bool haveAPet, int petNumber, string[] pets, int favColorNumber, string[] favColors) User;
+
+    Console.WriteLine("Введите ваше имя");
+    User.name = Console.ReadLine();
+
+    Console.WriteLine("Введите вашу фамилию");
+    User.lastName = Console.ReadLine();
+
+    Console.WriteLine("Введите ваш возраст");
+    string strAge = Console.ReadLine();
+
+    Console.WriteLine("Есть ли у вас питомец?");
+    User.haveAPet = true;
+
+    Console.WriteLine("Сколько у вас питомцев?");
+    string strPetNumber = Console.ReadLine();
+
+    Console.WriteLine("Сколько у вас любимых цветов?");
+    string strFavColorNumber = Console.ReadLine();
+
+    return User;
+}

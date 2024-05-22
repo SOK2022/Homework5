f8d30ed32f75203f73163f3f33c1c59599ae7fdf
Sergey <okulov.sergej2016@yandex.ru>
2024-05-23 00:28:23 +0500

    Добавили метод, выводящий информацию о пользователе

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+//EnterUser();
+ShowUser(EnterUser());
+
 static (string name, string lastName, int age, string[] pets, string[] favColors) EnterUser()
 {
     (string name, string lastName, int age,  string[] pets, string[] favColors) User;
@@ -20,18 +23,25 @@ static (string name, string lastName, int age, string[] pets, string[] favColors
     while (CheckNumber(strAge, out intAge));
     User.age = intAge;
 
-    Console.WriteLine("Есть ли у вас питомец?");
-
-    string strPetNumber;
-    int intPetNumber;
-    do
+    Console.WriteLine("Есть ли у вас питомец? Если есть, введите 'Да', иначе введите другое слово");
+    string havePet = Console.ReadLine();
+    if (havePet == "Да")
     {
-        Console.WriteLine("Сколько у вас питомцев?");
-        strPetNumber = Console.ReadLine();
+        string strPetNumber;
+        int intPetNumber;
+        do
+        {
+            Console.WriteLine("Сколько у вас питомцев?");
+            strPetNumber = Console.ReadLine();
+        }
+        while (CheckNumber(strPetNumber, out intPetNumber));
+        User.pets = PetsNames(intPetNumber);
     }
-    while (CheckNumber(strAge, out intPetNumber));
-    User.pets = PetsNames(intPetNumber);
-
+    else
+    {
+        User.pets = null;
+    }
+    
     string strFavColorNumber;
     int intFavColorNumber;
     do
@@ -39,11 +49,37 @@ static (string name, string lastName, int age, string[] pets, string[] favColors
         Console.WriteLine("Сколько у вас любимых цветов?");
         strFavColorNumber = Console.ReadLine();
     }
-    while (CheckNumber(strAge, out intFavColorNumber));
+    while (CheckNumber(strFavColorNumber, out intFavColorNumber));
     User.favColors = FavColors(intFavColorNumber);
 
     return User;
 }
+
+static void ShowUser((string name, string lastName, int age, string[] pets, string[] favColors) User)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Ваше имя: {User.name}");
+    Console.WriteLine($"Ваша фамилия: { User.lastName}");
+    Console.WriteLine($"Вам {User.age} лет");
+    if (User.pets != null)
+    {
+        Console.WriteLine($"У вас {User.pets.Length} питомцев:");
+        foreach ( var pet in User.pets)
+        {
+            Console.WriteLine(pet);
+        }
+    }
+    else
+    {
+        Console.WriteLine("У вас нет питомцев");
+    }
+    Console.WriteLine($"У вас {User.favColors.Length} любимых цветов:");
+    foreach (var favColor in User.favColors)
+    {
+        Console.WriteLine(favColor);
+    }
+}
+
 static bool CheckNumber(string strNumber, out int corrNumber)
 {
     if (int.TryParse(strNumber, out int number))
@@ -63,7 +99,7 @@ static string[] PetsNames(int number)
     string[] names = new string[number];
     for (int i = 0; i < names.Length; i++)
     {
-        Console.Write($"Имя питомца {i + 1}");
+        Console.WriteLine($"Имя питомца {i + 1}");
         names[i] = Console.ReadLine();
     }
     return names;
@@ -74,7 +110,7 @@ static string[] FavColors(int number)
     string[] colors = new string[number];
     for (int i = 0; i < colors.Length; i++)
     {
-        Console.Write($"Цвет {i + 1}");
+        Console.WriteLine($"Цвет {i + 1}");
         colors[i] = Console.ReadLine();
     }
     return colors;
